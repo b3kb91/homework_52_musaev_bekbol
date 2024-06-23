a293600efb6663d868ec17a578e8ee889bdacc41
Bekbol <19kani92@gmail.com>
2024-06-23 18:56:21 +0600

    добавил админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import ToDo
+
+admin.site.register(ToDo)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    description = models.TextField(max_length=500, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=50, null=False, blank=False, verbose_name="Статус", default="new")
+    date_completion = models.DateField(auto_now_add=False, null=True, blank=True, verbose_name="Дата выполнения")
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        db_table = 'todo_lists'
+        verbose_name = "Cписок задач"
+        verbose_name_plural = "Списки задач"
